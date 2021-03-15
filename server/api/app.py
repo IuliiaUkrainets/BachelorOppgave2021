ae1f5ce8b7d8a9cdffe81987ad86549cf3d308a8
IrinaBBB <balkarovairina@gmail.com>
2021-03-15 11:08:34 +0100

    add: initial database setup(Flask-SQLAlchemy)

--- server/api/app.py
+++ server/api/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from db import db
+from models.patient import PatientModel
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+@app.before_first_request
+def create_tables():
+    print("*********************************")
+    db.create_all()
+
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
