86860a37583cf48c0d4582f5d8787e99283088ad
IrinaBBB <balkarovairina@gmail.com>
2021-04-24 09:41:35 +0200

    add: edit user DTO/Controller + Cloudinary Service

--- server/api/API/Controllers/UsersController.cs
+++ server/api/API/Controllers/UsersController.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using API.DTOs;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -36,5 +38,29 @@ namespace API.Controllers
         {
             return await _userRepository.GetUserDtoAsync(username);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(UserUpdateDTO userUpdateDTO)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(userUpdateDTO, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        } 
+
+/* 
+        [HttpPost("add-photo")]
+        public async Task<UserPhotoDTO> AddPhoto(IFormFile file) 
+        {
+            
+            
+        }  */
+
     }
 }
---
+++ server/api/API/DTOs/UserUpdateDTO.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class UserUpdateDTO
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
